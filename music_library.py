15375f01dad6f49b002fc06cc0cdf13bcbcdcfe8
fahadhussain3 <fahad.hussain3133@gmail.com>
2024-08-15 11:57:31 +0500

    Add project files and configuration

---
+++ music_library.py
@@ -0,0 +1,5 @@
+music={
+    "jhol":"https://www.youtube.com/watch?v=-2RAq5o5pwc",
+    "winning speech":"https://www.youtube.com/watch?v=vsWxs1tuwDk",
+    "52 bars":"https://www.youtube.com/watch?v=4DfVxVeqk2o"
+}
